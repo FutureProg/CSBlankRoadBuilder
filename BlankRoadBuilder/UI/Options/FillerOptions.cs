c77a61b329efda487d037d36315e65343627ceab
Jad Hajjar <93581220+JadHajjar@users.noreply.github.com>
2022-12-11 10:48:17 +0200

    v0.3.1
    • Added default lane width customization inside Thumbnail Maker, these settings are shared between it and the game
    • Fixed the width calculation in Thumbnail Maker not taking into consideration lane counts
    • Fixed custom widths not being exported correctly with multiple lane counts
    • Fixed the Bi-directional 2L lanes in TM showing the wrong arrows in LHT mode
    • Fixed missing & out of position tram poles
    • Changed the maximum sizes of dashes to 10m

--- BlankRoadBuilder/UI/Options/FillerOptions.cs
+++ BlankRoadBuilder/UI/Options/FillerOptions.cs
@@ -50,11 +50,11 @@ internal class FillerOptions : OptionsPanelBase
 		dropDown.selectedIndex = enumVales.Keys.ToList().IndexOf((int)value.MarkingType);
 		xPos += 150 + 2 * Margin;
 
-		var dashSlider = AddSlider("Dash Length", xPos, 0.1F, 3F, 0.1F, value.DashLength, "m");
+		var dashSlider = AddSlider("Dash Length", xPos, 0.1F, 10F, 0.1F, value.DashLength, "m");
 		dashSlider.eventValueChanged += (s, v) => value.DashLength = v;
 		xPos += 160 + 2 * Margin;
 
-		var dashSpace = AddSlider("Dash Space", xPos, 0.1F, 3F, 0.1F, value.DashSpace, "m");
+		var dashSpace = AddSlider("Dash Space", xPos, 0.1F, 10F, 0.1F, value.DashSpace, "m");
 		dashSpace.eventValueChanged += (s, v) => value.DashSpace = v;
 
 		xPos = LeftMargin;
